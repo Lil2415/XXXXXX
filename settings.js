44fec46a8af5e7fc8e8e1ed4569b597ed05cb52f
Lil2415 <136098662+Lil2415@users.noreply.github.com>
2024-09-14 11:21:59 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "254706697688"
+global.ownername = "PERSONIFIED"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
-global.location = "Pakistan, Karachi, Karachi"
+global.location = "Nairobi, Kenya"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '254706697688'  //creator number
+global.ownername = 'PERSONIFIED' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
